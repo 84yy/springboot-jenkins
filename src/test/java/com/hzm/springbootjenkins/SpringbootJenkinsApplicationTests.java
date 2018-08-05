6d329a10eea3291a09bb55d252df449076655d28
Simon <baxi84yy@163.com>
2018-08-05 09:32:01 +0800

    Initial commit

---
+++ src/test/java/com/hzm/springbootjenkins/SpringbootJenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hzm.springbootjenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootJenkinsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
