ac7c7eee744fc14fc79e49ee6da19b6d1e970410
Simon <baxi84yy@163.com>
2018-08-07 11:29:32 +0800

    修改maven插件

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 echo "--------helloWorld------->>"
 pwd
+
+port=8100
+#根据端口号查询对应的pid
+pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
+#杀掉对应的进程，如果pid不存在，则不执行
+if [  -n  "$pid"  ];  then
+    kill  -9  $pid;
+fi
+
 cd /home/jenkins/target
 export JAVA_HOME='/usr/local/java/jdk1.8.0_181'
 PATH=$PATH:$JAVA_HOME/bin
 echo $JAVA_HOME
-java -jar springboot-jenkins-0.0.1-SNAPSHOT.jar
+nohup java -jar springboot-jenkins-0.0.1-SNAPSHOT.jar
