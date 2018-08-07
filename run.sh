e55ade45f919305e9e7ea75557c59292f443188d
Simon <baxi84yy@163.com>
2018-08-07 11:18:18 +0800

    修改maven插件

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 echo "--------helloWorld------->>"
 pwd
 cd /home/jenkins/target
-JAVA_HOME='/usr/local/java/jdk1.8.0_181'
+export JAVA_HOME='/usr/local/java/jdk1.8.0_181'
+PATH=$PATH:$JAVA_HOME/bin
 echo $JAVA_HOME
 java -jar springboot-jenkins-0.0.1-SNAPSHOT.jar
